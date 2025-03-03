47b8bdb628cd4373950a297bc56e27100d545fbe
Santosh0805 <sahum3378@gmail.com>
2025-03-04 02:14:41 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,7 @@ import { dbConnection } from "./database/dbConnection.js";
 const app = express();
 dotenv.config({ path: "./config.env" });
 
-app.use(
-  cors({
-    origin: ['http://localhost:5173'],
-    methods: ["POST"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
